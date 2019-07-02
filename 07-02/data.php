eca88ef42c1f8c0122819e638c3a319cf0e951d1
vytautas <vytautas.rimeikis@gmail.com>
2019-07-02 19:28:42 +0300

    mysql II wit excersize

---
+++ 07-02/data.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Turim dvi lenteles: products ir categories.
+ * Sukurtis Many To Many relation su foreign.
+ *
+ * Lenteliu stulpeliai:
+ *
+ * Table: products
+ * - id
+ * - title
+ * - slug
+ * - description
+ * - price
+ *
+ * Table: categories
+ * - id
+ * - title
+ * - slug
+ */
---
+++ 07-02/mysql_II.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * CONSTRAINT constraint_name
+FOREIGN KEY foreign_key_name (columns)
+REFERENCES parent_table(columns)
+ON DELETE action
+ON UPDATE action
+ */
+
+/*
+ * ALTER TABLE  `user_roles` ADD CONSTRAINT  `user_roles_role_id_foreign` FOREIGN KEY  `user_roles_role_id_foreign` (  `role_id` ) REFERENCES  `roles` (  `id` ) ON DELETE CASCADE ON UPDATE CASCADE
+ * ALTER TABLE `user_roles` DROP FOREIGN KEY `user_roles_role_id_foreign`;
+ * DELETE FROM `user_roles`
+WHERE NOT EXISTS (
+	SELECT 1 FROM `roles` WHERE `roles`.`id` = `user_roles`.`role_id`
+)
+ */

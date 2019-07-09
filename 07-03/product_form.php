865afb5107fb86adf554773882f77b4b2b1fb613
vytautas <vytautas.rimeikis@gmail.com>
2019-07-09 19:53:20 +0300

    categories to product

--- 07-03/product_form.php
+++ 07-03/product_form.php
@@ -2,19 +2,33 @@
 
 declare(strict_types = 1);
 
-require_once (__DIR__.'/db.php');
+require_once(__DIR__ . '/db.php');
 
-$formData = [];
+$formData = [
+        'category_ids' => [],
+];
+
+$sql = 'SELECT id, title FROM categories';
+
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$categories = $stmt->fetchAll();
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['product_id']) && $_GET['product_id'] > 0) {
     $productId = $_GET['product_id'];
 
-    $sql = 'SELECT * FROM products WHERE id= :productId';
+    $sql = 'SELECT p.*, GROUP_CONCAT(pc.category_id) AS category_ids
+        FROM products p
+        LEFT JOIN product_categories pc ON p.id = pc.product_id
+        WHERE p.id= :productId
+        GROUP BY p.id';
 
     $stmt = $conn->prepare($sql);
     $stmt->bindValue(':productId', $productId, PDO::PARAM_INT);
     $stmt->execute();
     $formData = $stmt->fetch();
+
+    $formData['category_ids'] = $formData['category_ids'] ? explode(',', $formData['category_ids']) : [];
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['save_data'])) {
@@ -30,6 +44,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['save_data'])) {
             $stmt->bindValue(':price', $postData['price'], PDO::PARAM_INT);
             $stmt->bindValue(':id', $postData['id'], PDO::PARAM_INT);
             $stmt->execute();
+
+            $sql = 'DELETE FROM product_categories WHERE product_id = :productId';
+            $stmt = $conn->prepare($sql);
+            $stmt->bindValue(':productId', $postData['id'], PDO::PARAM_INT);
+            $stmt->execute();
+
+            if (isset($postData['categories']) && !empty($postData['categories'])) {
+
+                $sql = 'INSERT INTO product_categories SET category_id = :categoryId, product_id = :productId';
+                foreach ($postData['categories'] as $categoryId) {
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bindValue(':productId', $postData['id'], PDO::PARAM_INT);
+                    $stmt->bindValue(':categoryId', $categoryId, PDO::PARAM_INT);
+                    $stmt->execute();
+                }
+            }
 
             header("Location: product.php");
 
@@ -47,14 +77,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['save_data'])) {
             $stmt->bindValue(':description', $postData['description'], PDO::PARAM_STR);
             $stmt->bindValue(':price', $postData['price'], PDO::PARAM_INT);
             $stmt->execute();
+
+            $productId = $conn->lastInsertId();
+
+            if ($productId > 0) {
+
+                if (isset($postData['categories']) && !empty($postData['categories'])) {
+
+                    $sql = 'INSERT INTO product_categories SET category_id = :categoryId, product_id = :productId';
+                    foreach ($postData['categories'] as $categoryId) {
+                        $stmt = $conn->prepare($sql);
+                        $stmt->bindValue(':productId', $productId, PDO::PARAM_INT);
+                        $stmt->bindValue(':categoryId', $categoryId, PDO::PARAM_INT);
+                        $stmt->execute();
+                    }
+                }
+
+                header("Location: product.php");
+            }
+
         } catch (Exception $exception) {
             echo $exception->getMessage();
             $formData = $postData;
         }
 
-        if ($conn->lastInsertId() > 0) {
-            header("Location: product.php");
-        }
     }
 }
 
@@ -80,7 +126,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['save_data'])) {
 
     <label for="title">Title</label>
     <br>
-    <input id="title" type="text" name="title" value="<?= isset($formData['title']) ? $formData['title'] : '' ?>" required>
+    <input id="title" type="text" name="title" value="<?= isset($formData['title']) ? $formData['title'] : '' ?>"
+           required>
 
     <br>
     <label for="slug">Slug</label>
@@ -90,12 +137,38 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['save_data'])) {
     <br>
     <label for="description">Description</label>
     <br>
-    <textarea id="description" name="description"><?= isset($formData['description']) ? $formData['description'] : '' ?></textarea>
+    <textarea id="description" name="description">
+        <?= isset($formData['description']) ? $formData['description'] : '' ?>
+    </textarea>
+
+    <br>
+    <label for="categories">Categories</label>
+    <fieldset>
+        <?php
+
+        foreach ($categories as $category) {
+            ?>
+
+            <input id="categories"
+                   type="checkbox"
+                   name="categories[]"
+                   value="<?= $category['id']; ?>"
+                <?= in_array($category['id'], $formData['category_ids']) ? 'checked="checked"' : ''?>
+            >
+            <?= $category['title']; ?>
+            <br>
+
+            <?php
+        }
+
+        ?>
+    </fieldset>
 
     <br>
     <label for="price">Price</label>
     <br>
-    <input id="price" type="number" name="price" value="<?= isset($formData['price']) ? $formData['price'] : 0 ?>" step="0.01" min="0" required>
+    <input id="price" type="number" name="price" value="<?= isset($formData['price']) ? $formData['price'] : 0 ?>"
+           step="0.01" min="0" required>
 
     <br>
     <br>
